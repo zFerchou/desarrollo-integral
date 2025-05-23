4edff8df6a32b7569c0eebf6611e6c95540e5d21
zFerchou <luisfernandoma94@gmail.com>
2025-05-22 19:37:58 -0600

    uwu

---
+++ src/componentes/App.jsx
@@ -0,0 +1,20 @@
+import NavBar from '../componentes/NavBar';
+
+function App() {
+    return(
+        <>
+
+        <NavBar />
+        <div>
+            <h2>Bienvenido a mi pagina web</h2>
+
+            <hr />
+            <p>
+                Este simple Aplicacion es creado conReact
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default App;
